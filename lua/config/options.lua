56978936912e76626b2a3fce505e00418265f9a2
Simon Hoyos <shmesa23@gmail.com>
2026-01-22 15:57:18 -0500

    feat(config): add search and window management options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,3 +12,13 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+
+-- Search Behavior
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Window Management
+vim.opt.splitbelow = true
+vim.opt.splitright = true
