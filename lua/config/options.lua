a8a76ad8c8100fa50be8831f5a18554443c932a0
Simon Hoyos <shmesa23@gmail.com>
2026-01-22 15:55:22 -0500

    feat(config): add indentation and formatting options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,9 @@ vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
 vim.opt.signcolumn = "yes"
+
+-- Indentation & Formatting
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
