5855748d082f4785a80e58aa1a7a489d823cc6d3
Simon Hoyos <shmesa23@gmail.com>
2026-01-22 17:44:01 -0500

    feat(treesitter): Add initial treesitter configuration

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = {},
+        sync_install = false,
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+}
