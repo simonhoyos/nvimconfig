be9235cf00d64a6d9815436ec42dbb656e630fd2
Simon Hoyos <shmesa23@gmail.com>
2026-01-22 20:17:52 -0500

    feat(lsp): Configure lua_ls with keymaps and format-on-save

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -8,9 +8,31 @@ local function setup_mason()
   })
 end
 
+local function setup_format_on_save(client, bufnr)
+  if client.supports_method("textDocument/formatting") then
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ bufnr = bufnr })
+      end,
+    })
+  end
+end
+
 local function setup_servers()
-  local lspconfig = require("lspconfig")
-  -- We will configure servers here later
+  local keymaps = require("plugins.lsp.keymaps")
+  local servers = require("plugins.lsp.servers")
+
+  local on_attach = function(client, bufnr)
+    keymaps.on_attach(client, bufnr)
+    setup_format_on_save(client, bufnr)
+  end
+
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+  servers.setup(on_attach, capabilities)
 end
 
 setup_mason()
---
+++ lua/plugins/lsp/servers.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+function M.setup(on_attach, capabilities)
+  local lspconfig = require("lspconfig")
+
+  lspconfig.lua_ls.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" },
+        },
+        workspace = {
+          library = vim.api.nvim_get_runtime_file("", true),
+          checkThirdParty = false,
+        },
+        telemetry = {
+          enable = false,
+        },
+      },
+    },
+  })
+end
+
+return M
