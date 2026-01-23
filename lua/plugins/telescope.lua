9baa39e40c2a57bf78c3b05527818e2757ba1b4a
Simon Hoyos <shmesa23@gmail.com>
2026-01-22 19:31:44 -0500

    feat(telescope): Add telescope and fzf-native configuration

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-fzf-native.nvim",
+    },
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+
+      telescope.setup({
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules",
+            "target",
+            "dist",
+            "%.git/",
+            "%.next/",
+            "%.data/",
+            "%.data%-crdb/",
+            "%.vercel/",
+            "%.venv/",
+          },
+          mappings = {
+            i = {
+              ["<C-k>"] = actions.move_selection_previous,
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            },
+          },
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
+        },
+      })
+
+      telescope.load_extension("fzf")
+    end,
+  },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+  },
+}
