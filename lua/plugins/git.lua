74e0bb5ad2688b9987a153e46dd1669a0ffed0bd
Simon Hoyos <shmesa23@gmail.com>
2026-01-25 14:51:48 -0500

    feat(git): Configure gitsigns signs and options

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -10,7 +10,37 @@ return {
   },
   {
     "lewis6991/gitsigns.nvim",
-    config = true,
+    opts = {
+      signs = {
+        add = { text = "│" },
+        change = { text = "│" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "┆" },
+      },
+      signcolumn = true,
+      numhl = false,
+      linehl = false,
+      word_diff = false,
+      watch_gitdir = {
+        interval = 1000,
+        follow_files = true,
+      },
+      attach_to_untracked = true,
+      current_line_blame = false,
+      sign_priority = 6,
+      update_debounce = 100,
+      status_formatter = nil,
+      max_file_length = 40000,
+      preview_config = {
+        border = "single",
+        style = "minimal",
+        relative = "cursor",
+        row = 0,
+        col = 1,
+      },
+    },
   },
   {
     "sindrets/diffview.nvim",
