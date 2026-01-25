3ac706a36e9a72ebf2611662f3b03019aaeb3354
Simon Hoyos <shmesa23@gmail.com>
2026-01-25 17:56:41 -0500

    feat(git): setup git interface
    * feat(git): Add neogit, gitsigns, and diffview plugins
    
    * feat(git): Configure gitsigns signs and options
    
    * feat(git): Configure neogit integrations
    
    * feat(git): Implement Neogit and Gitsigns keybindings
    
    * refactor(git): improve robustness and use lua api for keymaps
    
    - Guard keymap registration with checks for `neogit` and `gitsigns` to prevent errors if plugins are not loaded.
    - Switch `neogit` mappings to use direct Lua API calls instead of command strings.
    - Refactor `gitsigns` hunk navigation to better handle diff mode using `vim.cmd.normal`.
    - Add `buffer = true` to `gitsigns` keybindings.

---
+++ after/plugin/git.lua
@@ -0,0 +1,46 @@
+local neogit = package.loaded.neogit
+local gitsigns = package.loaded.gitsigns
+
+local opts = { silent = true, noremap = true, }
+
+if neogit then
+  vim.keymap.set("n", "<leader>gg", neogit.open,
+    vim.tbl_extend("force", opts, { desc = "[GIT] Neogit status", }))
+  vim.keymap.set("n", "<leader>gc", function() neogit.open({ "commit", }) end,
+    vim.tbl_extend("force", opts, { desc = "[GIT] Neogit commit", }))
+  vim.keymap.set("n", "<leader>gl", function() neogit.open({ "log", }) end,
+    vim.tbl_extend("force", opts, { desc = "[GIT] Neogit log", }))
+  vim.keymap.set("n", "<leader>gp", "<cmd>Neogit push<CR>",
+    vim.tbl_extend("force", opts, { desc = "[GIT] Neogit push", }))
+end
+
+if gitsigns then
+  vim.keymap.set("n", "<leader>gh", package.loaded.gitsigns.preview_hunk,
+    vim.tbl_extend("force", opts, { desc = "[GIT] Preview hunk", buffer = true, }))
+  vim.keymap.set("n", "<leader>gb",
+    package.loaded.gitsigns.toggle_current_line_blame,
+    vim.tbl_extend("force", opts,
+      { desc = "[GIT] Toggle line blame", buffer = true, }))
+
+  vim.keymap.set("n", "<leader>gj", function()
+    if vim.wo.diff then
+      return
+        vim.cmd.normal({ "]c", bang = true, })
+    end
+
+    vim.schedule(gitsigns.next_hunk)
+
+    return "<Ignore>"
+  end, { expr = true, desc = "[GIT] Next hunk", buffer = true, })
+
+  vim.keymap.set("n", "<leader>gk", function()
+    if vim.wo.diff then
+      return
+        vim.cmd.normal({ "[c", bang = true, })
+    end
+
+    vim.schedule(gitsigns.prev_hunk)
+
+    return "<Ignore>"
+  end, { expr = true, desc = "[GIT] Previous hunk", buffer = true, })
+end
---
+++ lua/plugins/git.lua
@@ -0,0 +1,55 @@
+return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {
+      disable_commit_confirmation = true,
+      integrations = {
+        diffview = true,
+        telescope = true,
+      },
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "│" },
+        change = { text = "│" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "┆" },
+      },
+      signcolumn = true,
+      numhl = false,
+      linehl = false,
+      word_diff = false,
+      watch_gitdir = {
+        interval = 1000,
+        follow_files = true,
+      },
+      attach_to_untracked = true,
+      current_line_blame = false,
+      sign_priority = 6,
+      update_debounce = 100,
+      status_formatter = nil,
+      max_file_length = 40000,
+      preview_config = {
+        border = "single",
+        style = "minimal",
+        relative = "cursor",
+        row = 0,
+        col = 1,
+      },
+    },
+  },
+  {
+    "sindrets/diffview.nvim",
+    config = true,
+  },
+}
