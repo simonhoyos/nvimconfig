b730dc6635875fd233ba477e06ea604e328ab897
Simon Hoyos <shmesa23@gmail.com>
2026-02-13 16:32:09 -0500

    feat(testing): migrate from vim-test to neotest
    - Replace vim-test with neotest as the primary testing framework
    - Add support for RSpec and Vitest via neotest adapters
    - Update test keybindings to use neotest API
    - Add new keymaps for test summary toggle and opening test output

--- lua/plugins/testing.lua
+++ lua/plugins/testing.lua
@@ -1,28 +1,42 @@
 return {
   {
-    "vim-test/vim-test",
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "olimorris/neotest-rspec",
+      "marilari88/neotest-vitest",
+    },
     config = function()
-      local opts = { buffer = true, silent = true, noremap = true, }
+      require("neotest").setup({
+        adapters = {
+          require("neotest-rspec"),
+          require("neotest-vitest"),
+        },
+      })
 
-      vim.g["test#strategy"] = "neovim"
-      vim.g["test#neovim#term_position"] = "vertical"
-      vim.g["test#neovim#start_normal"] = 1
-      vim.g["test#basic#start_normal"] = 1
+      local opts = { silent = true, noremap = true, }
 
-      vim.keymap.set("n", "<leader>tt", "<cmd>TestNearest<CR>",
+      vim.keymap.set("n", "<leader>tt",
+        function() require("neotest").run.run() end,
         vim.tbl_extend("force", opts, { desc = "[TEST] Run nearest test", }))
-      vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<CR>",
+      vim.keymap.set("n", "<leader>tf",
+        function() require("neotest").run.run(vim.fn.expand("%")) end,
         vim.tbl_extend("force", opts, { desc = "[TEST] Run all tests in file", }))
-      vim.keymap.set("n", "<leader>ts", "<cmd>TestSuite<CR>",
+      vim.keymap.set("n", "<leader>ts",
+        function() require("neotest").run.run(vim.fn.getcwd()) end,
         vim.tbl_extend("force", opts, { desc = "[TEST] Run test suite", }))
-      vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<CR>",
+      vim.keymap.set("n", "<leader>tl",
+        function() require("neotest").run.run_last() end,
         vim.tbl_extend("force", opts, { desc = "[TEST] Run last run test", }))
-      vim.keymap.set("n", "<leader>tv", "<cmd>TestVisit<CR>",
-        vim.tbl_extend("force", opts, { desc = "[TEST] Visit test", }))
-
-      -- let g:test#custom_strategies = {'jest': function('JestStrategy')}
-      -- nnoremap <leader>dt :TestNearest -strategy=jest<CR>
-      -- ]])
+      vim.keymap.set("n", "<leader>tv",
+        function() require("neotest").summary.toggle() end,
+        vim.tbl_extend("force", opts, { desc = "[TEST] Toggle summary", }))
+      vim.keymap.set("n", "<leader>to",
+        function() require("neotest").output.open({ enter = true, }) end,
+        vim.tbl_extend("force", opts, { desc = "[TEST] Open test output", }))
     end,
   },
 }
